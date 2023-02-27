1731790a1720b56f5758b100a752860c5310c567
Webmyn001 <bellomuhyideen0001@gmail.com>
2023-02-27 09:00:21 -0800

    d

--- Routes/lessonRoutes.js
+++ Routes/lessonRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const Lesson = require ("../models/lessonModel")
-const multer = require('multer')
 
 const {createLesson,
     deleteLesson,
@@ -13,23 +12,7 @@ const {createLesson,
 
 const router = express.Router()
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "Images")
-  },
-  filename: function(req, file, cb){
-    cb(null, file.originalname)
-  }
-})
 
-
-
-  let upload = multer({storage : storage})
-  const multipleUpload = upload.fields([
-    {name : "IDcardimage",  maxCount: 1 },
-    {name: "Jambimage",  maxCount: 1 }
-
-])
 
 
 router.post('/add', createLesson)
